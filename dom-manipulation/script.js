7a2d29000d0f23d74b308f4c11968f4bf99cf8c7
Gideonkiprut <gideonkipruto695@gmail.com>
2024-07-24 23:20:27 -0700

    update script.js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -10,14 +10,16 @@ function showRandomQuote() {
     const quoteDisplay = document.getElementById('quoteDisplay');
     const randomIndex = Math.floor(Math.random() * quotes.length);
     const randomQuote = quotes[randomIndex];
-    quoteDisplay.textContent = `${randomQuote.text} - ${randomQuote.category}`;
+
+    // Update quote display using innerHTML
+    quoteDisplay.innerHTML = `${randomQuote.text} - ${randomQuote.category}`;
 }
 
 // Function to add a new quote
 function addQuote() {
     const newQuoteText = document.getElementById('newQuoteText').value;
     const newQuoteCategory = document.getElementById('newQuoteCategory').value;
-    
+
     if (newQuoteText && newQuoteCategory) {
         quotes.push({ text: newQuoteText, category: newQuoteCategory });
         document.getElementById('newQuoteText').value = '';
